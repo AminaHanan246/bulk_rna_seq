8b81f6a06b065df08827ef6748b162e4c8d8ca82
AminaHanan246 <aminahanaan0310@gmail.com>
2025-08-13 03:04:31 +0530

    Performed DESeq and stored the values in csv.

--- bulk RNA seq.R
+++ bulk RNA seq.R
@@ -1,12 +1,12 @@
-#data:https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE272922
-#=================================
+#==========================================
 #Load required Packages
-#=================================
+#==========================================
 install.packages("DESeq2")
 library("DESeq2")
-#=================================
+library(zoo)
+#==========================================
 #set paths and Sample info
-#==================================
+#==========================================
 setwd("D:/bioinformatic/bioinfo_prj/Bulk RNA seq")
 #load the count datas obtained fron Htseq; check.names FALSE to remain the same name
 count_data = read.csv(file = "TCGA-count-data.csv", header = TRUE, sep = "," , row.names = 1, check.names = FALSE )
@@ -20,14 +20,45 @@ rownames(col_data)
 colnames(count_data)
 all(rownames(col_data)==colnames(count_data)) #ensure all samples are labelled
 
-#===========================================
+#==========================================
 #Data cleaning
-#===========================================
+#==========================================
 class(count_data)
 is.na(count_data) #check missing values
 which(is.na(count_data),arr.ind = T)
-install.packages("zoo")
-library(zoo)
 #mean of gene values across sample are taken to fill in missing value
 count_data[] = t(na.aggregate(t(count_data)))
 count_data
+
+#==========================================
+# DESeq - for differential expressed genes
+#==========================================
+#convert categorical data to factor
+col_data$condition <- factor(col_data$condition)cou
+#Create DESeq Dataset
+dds = DESeqDataSetFromMatrix(countData = round(count_data),
+                              colData = col_data,
+                              design = ~ condition)
+#Set reference level will compare cancer to normal
+dds$condition <- relevel(dds$condition, ref = "normal")
+dds
+
+#Run DESeq
+dds <- DESeq(dds)
+res1 <- results(dds)
+summary(res1)
+
+#==========================================
+#DEGs
+#==========================================
+#highly up-regulated genes 
+resSigup <- subset(res1, padj <0.05 & log2FoldChange>1)
+write.csv(resSigup, "upregulated_genes.csv")
+
+#highly down regulated genes 
+resSigdwn <- subset(res1, padj <0.05 & log2FoldChange< -1)
+write.csv(resSigdwn, "downregulated_genes.csv")
+
+#for all differentially expressed genes
+resSig <- subset(res1, padj<0.05 & log2FoldChange>1 | padj<0.05 & log2FoldChange< -1)
+write.csv(resSig, "DE_genes.csv")

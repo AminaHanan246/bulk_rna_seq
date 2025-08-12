fde5cf2dca2c9dc7a7f70b4c2dbce3f7fe9c75f8
AminaHanan246 <aminahanaan0310@gmail.com>
2025-08-13 04:35:13 +0530

    Visualised DEGs using Enhanced volcano plot.

--- bulk RNA seq.R
+++ bulk RNA seq.R
@@ -59,6 +59,50 @@ write.csv(resSigup, "upregulated_genes.csv")
 resSigdwn <- subset(res1, padj <0.05 & log2FoldChange< -1)
 write.csv(resSigdwn, "downregulated_genes.csv")
 
-#for all differentially expressed genes
+#for all differential expressed genes
 resSig <- subset(res1, padj<0.05 & log2FoldChange>1 | padj<0.05 & log2FoldChange< -1)
 write.csv(resSig, "DE_genes.csv")
+
+#==========================================
+#Enseble IDs to gene symbols
+#==========================================
+
+#==========================================
+#Data visualisation
+#==========================================
+if(!require("BiocManager", quietly = T ))
+  install.packages("BiocManager")
+BiocManager::install("EnhancedVolcano")
+library(EnhancedVolcano)
+
+EnhancedVolcano(res1,                        
+  lab = "",                    # Labels for each point (empty = no labels shown)
+  x = 'log2FoldChange',        
+  y = 'pvalue',                
+  selectLab = NULL,            # label gene of interest
+  title = NULL,                # plot title
+  cutoffLineType = 'twodash',  # Style of threshold lines
+  cutoffLineWidth = 0.8,       # Thickness of threshold lines
+  xlim = c(-8,8),              # x-axis limits
+  xlab = bquote(~Log[2]~ 'fold change'),  
+  ylim = c(0,12),              # y-axis limits
+  ylab = bquote(~-Log[10]~italic(P)),     
+  pCutoff = 0.05,              # p-value threshold for significance
+  FCcutoff = 1.0,              # Fold change threshold for biological relevance
+  #transcriptPointSize = 0.5,  # size of points
+  #transcriptLabSize = 4.0,    # size of labels
+  colAlpha = 1,                # Transparency of points (1 = opaque)
+  shape = 19,                  # Shape of points (19 = solid circle)
+  subtitle = NULL,             # Optional: subtitle
+  legendPosition = 'top',      # Position of legend
+  legendLabSize = 12,          # Font size of legend labels
+  legendIconSize = 4.0,        # Size of legend icons
+  gridlines.major = FALSE,     # Hide major gridlines
+  gridlines.minor = FALSE,     # Hide minor gridlines
+  drawConnectors = FALSE,      # Don't draw lines from labels to points
+  widthConnectors = 0.2,       # Connector line thickness (if enabled)
+  colConnectors = 'grey50',    # Connector line color
+  border = 'full'              # Border style around plot
+) 
+
+png(file = "DE_volcano.png")
